e50cda5ed62ffd2a95723edae39b29ea87fc76be
nm1077 <nm1077@gmail.com>
2018-08-12 23:13:15 -0700

    update server.js

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,45 @@
+const { ObjectID } = require('mongodb')
+
+const { mongoose } = require('./../server/db/mongoose')
+const { Todo } = require('./../server/models/todo')
+const { User } = require('./../server/models/user')
+
+var id = '5b70938ae6b300ed984ecf10'
+
+if (!ObjectID.isValid(id)) {
+  console.log('Id not valid')
+}
+
+User.findById(id).then(
+  user => {
+    if (!user) {
+      console.log('User not found')
+    }
+    console.log('User by Id', user)
+  },
+  e => console.log(e)
+)
+// var id = '5b70af493177dbbc43df612a'
+
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid')
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos)
+// })
+
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todos', todo)
+// })
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found')
+//   }
+//   console.log('Todo by Id', todo)
+// }).catch((e) => console.log(e))
---
+++ server/tests/server.test.js
@@ -0,0 +1,109 @@
+const expect = require('expect')
+const request = require('supertest')
+const { ObjectID } = require('mongodb')
+
+const { app } = require('./../server')
+const { Todo } = require('./../models/todo')
+
+const todos = [
+  {
+    _id: new ObjectID(),
+    text: 'first todo'
+  },
+  {
+    _id: new ObjectID(),
+    text: 'second todo'
+  }
+]
+
+beforeEach(done => {
+  Todo.remove({})
+    .then(() => {
+      Todo.insertMany(todos)
+    })
+    .then(() => done())
+})
+
+describe('POST /todos', () => {
+  it('should create a new todo', done => {
+    var text = 'test todo text'
+
+    request(app)
+      .post('/todos')
+      .send({ text })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.text).toBe(text)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        Todo.find()
+          .then(todos => {
+            expect(todos.length).toBe(3)
+            expect(todos[2].text).toBe(text)
+            done()
+          })
+          .catch(e => done(e))
+      })
+  })
+
+  it('should not create a todo if not valid', done => {
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        Todo.find()
+          .then(todos => {
+            expect(todos.length).toBe(2)
+            done()
+          })
+          .catch(e => done())
+      })
+  })
+})
+
+describe('GET /todos', () => {
+  it('should get all todos', done => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2)
+      })
+      .end(done)
+  })
+})
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', done => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(todos[0].text)
+      })
+      .end(done)
+  })
+
+  it('should return 404 if todo not found', done => {
+    var hexId = new ObjectID().toHexString()
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done)
+  })
+  it('should return 404 for non-object id', done => {
+    request(app)
+      .get('/todos/123')
+      .expect(404)
+      .end(done)
+  })
+})
